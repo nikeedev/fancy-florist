183c5918dac163668de88765e45c81e103bacadb
nikeedev <myftr12394@gmail.com>
2022-05-22 11:38:28 +0200

    Updates!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Fancy Florist
 
-A GameEngine.js game, and made for imporoving the engine itself!
+A GameEngine.js game, and made for improving the engine itself!
 
 Try at https://nikeedev.github.io/fancy-florist
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-
-
-
-import { Vector2, Rect, Size, Screen, Object, Sprite, version, Input, Keys, info } from '../node_modules/@nikee_dev/gameengine_js/GameEngine.js'
+import { GameEngine } from '../node_modules/@nikee_dev/gameengine_js/GameEngine.js'
 
 const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-const ScreenSize = new Size(canvas.width, canvas.height);
+const ScreenSize = new GameEngine.Size(canvas.width, canvas.height);
 
 
