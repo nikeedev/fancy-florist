5d00543f1dedf460e3925f015e7c6f3fabad90b8
nikeedev <myftr12394@gmail.com>
2023-02-20 11:11:10 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,52 @@
-import { Playlib } from "../node_modules/playlib/bin/playlib.js";
+import { Playlib } from "../../playlib.js/bin/playlib.js";
 
-const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas");
+const canvas: any = document.getElementById("canvas");
 canvas.width = window.innerWidth - 40
 canvas.height = window.innerHeight - 30
-const ScreenSize = new Playlib.Size(canvas.width, canvas.height);
+const ScreenSize = new Playlib.Vector2(canvas.width, canvas.height);
 
 let config = {
     game_name: "Fancy Florist",
     game_version: "1.0.0",
     style: "border: 1px solid black; background-color: white;",
-    width: ScreenSize.width,
-    height: ScreenSize.height,
+    width: ScreenSize.x,
+    height: ScreenSize.y,
     useOwnCanvas: true,
     canvas: canvas,
 }
 
 // var player = new Playlib.Rect(new PlayLib.Vector2(60, 60), new PlayLib.Size(20, 20), ScreenSize);
 
-var player = new Playlib.Sprite("./assets/player_canvas.png", new Playlib.Vector2(config.width/2-70/2, config.height/2-70/2), ScreenSize, null);
-
-await player.init();
+var player = new Playlib.Sprite("./assets/player_canvas.png", new Playlib.Vector2(config.width/2-70/2, config.height/2-70/2), ScreenSize);
+console.log(player);
+const speed = 15
 
 class GameScene extends Playlib.Scene
 {
 
-    create(ctx: any)
-    {
+    async create(ctx: any)
+    {        
+        this.ClearScreen = true;
+
+        await player.init();
+
         player.draw(ctx);
     }
 
-    update(ctx: any, dt: number) 
+    async update(ctx: any, dt: number) 
     {
     
         if (Playlib.Input.KeyPressed(Playlib.Keys.Up)) {
-            player.position.y -= 2.5;
+            player.position.y -= speed;
         }
         if (Playlib.Input.KeyPressed(Playlib.Keys.Down)) {
-            player.position.y += 2.5;
+            player.position.y += speed;
         }
         if (Playlib.Input.KeyPressed(Playlib.Keys.Left)) {
-            player.position.x -= 2.5;
+            player.position.x -= speed;
         }
         if (Playlib.Input.KeyPressed(Playlib.Keys.Right)) {
-            player.position.x += 2.5;
+            player.position.x += speed;
         }
 
         if (player.position.y <= 0) {
@@ -52,16 +56,16 @@ class GameScene extends Playlib.Scene
             player.position.x = 1;
         }
 
-        if (player.position.y >= ScreenSize.height - 70) {
-            player.position.y = 1 - 70 + ScreenSize.height;
+        if (player.position.y >= ScreenSize.y - 70) {
+            player.position.y = 1 - 70 + ScreenSize.y;
         }
         
         
-        if (player.position.x >= ScreenSize.width - 70) {
-            player.position.x = 1 - 70 + ScreenSize.width;
+        if (player.position.x >= ScreenSize.x - 70) {
+            player.position.x = 1 - 70 + ScreenSize.x;
         }
 
-        player.draw(ctx);
+        player.draw(ctx, new Playlib.Vector2(34, 63), new Playlib.Vector2(1, 1), new Playlib.Vector2(34, 63));
     }
 }
 
